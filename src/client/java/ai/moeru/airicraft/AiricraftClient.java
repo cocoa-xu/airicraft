3cf42fe21ee1a90155f520cb7f8cac9044ca7f0e
Rin <shinohara-rin@users.noreply.github.com>
2026-03-21 15:14:54 +0800

    init

---
+++ src/client/java/ai/moeru/airicraft/AiricraftClient.java
@@ -0,0 +1,10 @@
+package ai.moeru.airicraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AiricraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
